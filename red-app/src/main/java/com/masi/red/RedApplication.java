40bcd26a3df016dcf68450bf308a46d272e97685
polszewsky <32812297+polszewsky@users.noreply.github.com>
2019-03-29 20:48:05 +0100

    crud - dalej

--- red-app/src/main/java/com/masi/red/RedApplication.java
+++ red-app/src/main/java/com/masi/red/RedApplication.java
@@ -2,10 +2,8 @@ package com.masi.red;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(scanBasePackages = "com.masi.red")
+@SpringBootApplication(scanBasePackages = "com.masi.red.*")
 public class RedApplication {
 
     public static void main(String[] args) {
