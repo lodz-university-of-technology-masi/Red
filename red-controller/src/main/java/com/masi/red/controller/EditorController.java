72d59dba6f84d29d7892e508238fb07e59783541
polszewsky <32812297+polszewsky@users.noreply.github.com>
2019-03-30 13:34:44 +0100

    skończony crud dla editora

--- red-controller/src/main/java/com/masi/red/controller/EditorController.java
+++ red-controller/src/main/java/com/masi/red/controller/EditorController.java
@@ -1,6 +1,8 @@
 package com.masi.red.controller;
 
 
+import com.masi.red.EditorService.EditorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,32 +12,39 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/redaktor")
 public class EditorController {
 
+    @Autowired
+    EditorService editorService;
+
     @PutMapping( value="{editorName}/{editorSurname}")
     public ResponseEntity<String> createEditor(@PathVariable String editorName, @PathVariable String editorSurname){
 
         String response = editorName + " " + editorSurname;
 
-        return new ResponseEntity<>("Created Editor: " + response, HttpStatus.OK);
+        Integer editorID = editorService.createEditor(editorName,editorSurname);
+
+        return new ResponseEntity<>("Created Editor ID : " + editorID, HttpStatus.OK);
     }
 
     @GetMapping( value="{editorId}/")
     public ResponseEntity<Object> readEditor(@PathVariable Integer editorId){
 
-        //Object editor = editorService.getEditorByID(editorId);
-        Object editor = editorId.toString();
+        Object editor = editorService.readEditor(editorId);
 
         return new ResponseEntity<>(editor, HttpStatus.OK);
     }
 
     @PostMapping( value="{editorId}/{editorName}/{editorSurname}")
-    public void updateEditor(@PathVariable Integer editorId, @PathVariable String editorName, @PathVariable String editorSurname){
+    public ResponseEntity<String> updateEditor(@PathVariable Integer editorId, @PathVariable String editorName, @PathVariable String editorSurname){
 
+        editorService.updateEditor(editorId, editorName, editorSurname);
+
+        return new ResponseEntity<>("Aktualizacja Redaktora", HttpStatus.OK);
     }
 
     @DeleteMapping( value="{editorId}")
     public ResponseEntity<String> deleteEditor(@PathVariable Integer editorId){
 
-        //Object editor = editorService.getEditorByID(editorId);
+        editorService.deleteEditor(editorId);
 
         return new ResponseEntity<>("Usunięto Redaktora o ID : " + editorId, HttpStatus.OK);
     }
