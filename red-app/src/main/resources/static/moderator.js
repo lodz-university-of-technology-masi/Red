463cca252b8b4684e8b009858a0fd3505dac5a68
Piotr196 <stanczyktom@gmail.com>
2019-04-01 01:08:25 +0200

    temp

--- red-app/src/main/resources/static/moderator.js
+++ red-app/src/main/resources/static/moderator.js
@@ -206,3 +206,99 @@ $(document).ready(function() {
         });
     });
 });
+
+//***************************************************************
+//  A P I       T E S T S
+//***************************************************************
+
+test_api = "/tests";
+
+$(document).ready(function() {
+
+    $("#FormCreateTestButton").click(function () {
+
+        var dataForm = $('#FormCreateTest').serializeArray();
+
+        var jsonObject = {};
+        jsonObject["jobTitle"] = dataForm[0].value;
+
+
+        console.log(JSON.stringify(jsonObject));
+
+        $.ajax({
+            type: "POST",
+            contentType: "application/json",
+            url: test_api,
+            data: JSON.stringify(jsonObject),
+            dataType: 'json',
+            success: function (data) {
+                console.log("response: " + data);
+
+                setTimeout(function () {
+                    window.location.reload(true);
+                }, 2000);
+            },
+            error: function (e) {
+                console.log("ERROR : ", e);
+            }
+        });
+    });
+
+    $("#FormUpdateTestButton").click(function () {
+
+        var dataForm = $('#FormUpdateTest').serializeArray();
+
+        var jsonObject = {};
+        jsonObject["id"] = dataForm[0].value;
+        jsonObject["jobTitle"] = dataForm[1].value;
+
+        console.log(JSON.stringify(jsonObject));
+
+        $.ajax({
+            type: "PUT",
+            contentType: "application/json",
+            url: test_api + "/" + dataForm[0].value,
+            data: JSON.stringify(jsonObject),
+            dataType: 'json',
+            success: function (data) {
+                console.log(data);
+
+                setTimeout(function () {
+                    window.location.reload(true);
+                }, 2000);
+            },
+            error: function (e) {
+                console.log("ERROR : ", e);
+            }
+        });
+    });
+
+    $(".deleteTestButton").click(function (event) {
+
+        console.log(event.target.id);
+
+        var testID = event.target.id;
+        console.log("ID: " + testID[1]);
+
+
+        $.ajax({
+            type: "DELETE",
+            url: test_api + "/" + testID[1],
+            success: function (data) {
+                console.log(data);
+
+                setTimeout(function () {
+                    window.location.reload(true);
+                }, 2000);
+            },
+            error: function (e) {
+                console.log("ERROR : ", e);
+            }
+        });
+
+    });
+
+    $(".addQuestionToTestButton").click(function () {
+       $("#questionsToTestCreate")[0].innerHTML += $('#QuestionIdCreateInput')[0].value + ', ';
+    });
+});
