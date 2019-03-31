a25d91b2404623dffcc69a53017e0517f0e4ad07
Jakub <jakub.kalucki96@gmail.com>
2019-03-31 22:16:40 +0200

    add annotation for a constructor to Editor class

--- red-model/src/main/java/com/masi/red/entity/Editor.java
+++ red-model/src/main/java/com/masi/red/entity/Editor.java
@@ -1,18 +1,12 @@
 package com.masi.red.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Editor {
-
     private int id;
     private String name;
     private String surname;
-
-    public Editor(int id, String name, String surname)
-    {
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-    }
 }
