c121fdf17c315a0c6ae1d10ad5d32ffd32fd96f1
polszewsky <32812297+polszewsky@users.noreply.github.com>
2019-03-31 23:55:10 +0200

    hotfix frontu

--- red-security/src/main/java/com/masi/red/WebSecurityConfig.java
+++ red-security/src/main/java/com/masi/red/WebSecurityConfig.java
@@ -13,6 +13,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/").permitAll();
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/resources/**").permitAll();
     }
 }
