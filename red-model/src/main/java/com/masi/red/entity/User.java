6f041c4642e07de0378a5d3e37434506f0485da1
Jakub <jakub.kalucki96@gmail.com>
2019-04-01 03:14:07 +0200

    enable http basic auth

--- red-model/src/main/java/com/masi/red/entity/User.java
+++ red-model/src/main/java/com/masi/red/entity/User.java
@@ -3,6 +3,7 @@ package com.masi.red.entity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,6 +15,7 @@ import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "users")
@@ -67,17 +69,17 @@ public class User implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
