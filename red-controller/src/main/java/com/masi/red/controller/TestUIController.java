aabe772c5c83d6d7473f90639dca6a311e7f35c5
polszewsky <32812297+polszewsky@users.noreply.github.com>
2019-04-22 20:05:02 +0200

    stworzona stroina rozwiązywania testu

---
+++ red-controller/src/main/java/com/masi/red/controller/TestUIController.java
@@ -0,0 +1,21 @@
+package com.masi.red.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class TestUIController {
+
+    @GetMapping(value = "/kandydat/{jobID}")
+    public String getJobTestPage(@PathVariable Integer jobID, @RequestParam String language, Model model){
+
+        model.addAttribute("job", jobID);
+
+        return "testpage";
+    }
+
+}
