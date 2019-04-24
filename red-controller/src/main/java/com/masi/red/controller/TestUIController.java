d91eca81db4f7b970737de58b57f5b347e2350d9
polszewsky <32812297+polszewsky@users.noreply.github.com>
2019-04-24 23:37:21 +0200

    poprawki...

--- red-controller/src/main/java/com/masi/red/controller/TestUIController.java
+++ red-controller/src/main/java/com/masi/red/controller/TestUIController.java
@@ -1,6 +1,9 @@
 package com.masi.red.controller;
 
 
+import com.masi.red.JobTitleService;
+import com.masi.red.entity.JobTitle;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +13,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class TestUIController {
 
-    @GetMapping(value = "/kandydat/stanowisko/{jobID}")
-    public String getJobTestPage(@PathVariable Integer jobID, @RequestParam String language, Model model){
+    @Autowired
+    JobTitleService jobService;
 
-        model.addAttribute("job", jobID);
+    @GetMapping(value = "/kandydat/stanowisko/{jobId}")
+//    public String getJobTestPage(@PathVariable Integer jobId, @RequestParam String language, Model model){
+        public String getJobTestPage(@PathVariable Integer jobId, Model model){
+        JobTitle jobTitle = jobService.getJobTitleById(jobId);
+
+        model.addAttribute("jobTitle", jobTitle);
 
         return "testpage";
     }
-
 }
