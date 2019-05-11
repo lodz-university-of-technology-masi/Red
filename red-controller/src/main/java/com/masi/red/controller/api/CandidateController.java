46a24a27c67165c6d2c9a6ce9ac075917bc09837
Jakub <jakub.kalucki96@gmail.com>
2019-05-11 21:08:04 +0200

    remove useless annotation

--- red-controller/src/main/java/com/masi/red/controller/api/CandidateController.java
+++ red-controller/src/main/java/com/masi/red/controller/api/CandidateController.java
@@ -1,12 +1,10 @@
 package com.masi.red.controller.api;
 
-
 import com.masi.red.IAnswerService;
 import com.masi.red.IUserService;
 import com.masi.red.dto.CandidateAnswerDTO;
 import com.masi.red.dto.UserDto;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@Slf4j
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class CandidateController {
 
