effafdbc84a8b508bbabb14b105fb243a077a53c
Jakub <jakub.kalucki96@gmail.com>
2019-05-26 23:52:26 +0200

    support scale question type when exporting to csv

--- red-model/src/main/java/com/masi/red/common/CsvConstants.java
+++ red-model/src/main/java/com/masi/red/common/CsvConstants.java
@@ -8,6 +8,8 @@ public class CsvConstants {
     public static final String DEFAULT_ARRAY_OPEN = "[";
     public static final String DEFAULT_ARRAY_CLOSE = "]";
     public static final String DEFAULT_SUGGESTED_ANSWER_MARKER = "|";
+    public static final String DEFAULT_POSSIBLE_ANSWERS_DELIMITER = ",";
+    public static final String DEFAULT_POSSIBLE_ANSWERS_NUMBER = "1";
 
     private CsvConstants() {
     }
--- red-service/src/main/java/com/masi/red/CsvService.java
+++ red-service/src/main/java/com/masi/red/CsvService.java
@@ -4,28 +4,32 @@ import com.masi.red.common.CsvConstants;
 import com.masi.red.common.QuestionType;
 import com.masi.red.common.QuestionTypeMapper;
 import com.masi.red.entity.Question;
+import com.masi.red.entity.ScaleQuestion;
 import com.masi.red.entity.SingleChoiceQuestion;
 import com.masi.red.entity.Test;
 import com.masi.red.helper.EntityFinder;
 import com.opencsv.CSVWriter;
 import com.opencsv.ICSVWriter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class CsvService implements ICsvService {
 
     private static final String[] TEST_CSV_HEADER =
             {"numer", "typ", "jezyk", "tresc", "liczba_mozliwych_odpowiedzi", "[tresc_kazdej_odpowiedzi]"};
-    private static final String DEFAULT_POSSIBLE_ANSWERS_NUMBER = "1";
 
     private final EntityFinder entityFinder;
 
@@ -63,37 +67,66 @@ public class CsvService implements ICsvService {
     }
 
     private String getCsvPossibleAnswers(Question question, String type) {
-        if (type.equals(QuestionType.SINGLE_CHOICE.getSymbol())) {
-            return getSingleChoiceQuestionCsvAnswers(question);
+        String suggestedAnswer;
+        if (Objects.equals(type, QuestionType.SINGLE_CHOICE.getSymbol())) {
+            suggestedAnswer = getSingleChoiceQuestionCsvAnswers(question);
+        } else if (Objects.equals(type, QuestionType.SCALE.getSymbol())) {
+            suggestedAnswer = getScalaQuestionCsvAnswers(question);
+        } else {
+            suggestedAnswer = question.getSuggestedAnswer() == null ? Strings.EMPTY : question.getSuggestedAnswer();
+            suggestedAnswer += CsvConstants.DEFAULT_SUGGESTED_ANSWER_MARKER;
         }
-        String suggestedAnswer = question.getSuggestedAnswer() == null ? Strings.EMPTY : question.getSuggestedAnswer();
         return CsvConstants.DEFAULT_ARRAY_OPEN
                 + suggestedAnswer
-                + CsvConstants.DEFAULT_SUGGESTED_ANSWER_MARKER
                 + CsvConstants.DEFAULT_ARRAY_CLOSE;
     }
 
-    private String getSingleChoiceQuestionCsvAnswers(Question question) {
-        StringBuilder answerContent = new StringBuilder(CsvConstants.DEFAULT_ARRAY_OPEN);
-        for (Iterator<String> iterator = ((SingleChoiceQuestion) question).getPossibleAnswers().iterator(); iterator.hasNext(); ) {
-            String possibleAnswer = iterator.next();
-            answerContent.append(possibleAnswer);
-            if (possibleAnswer.equals(question.getSuggestedAnswer())) {
-                answerContent.append(CsvConstants.DEFAULT_SUGGESTED_ANSWER_MARKER);
-            }
-            if (iterator.hasNext()) {
-                answerContent.append(ICSVWriter.DEFAULT_SEPARATOR);
-            }
+    private String getScalaQuestionCsvAnswers(Question question) {
+        double minValue = ((ScaleQuestion) question).getMinValue();
+        double maxValue = ((ScaleQuestion) question).getMaxValue();
+        double interval = ((ScaleQuestion) question).getInterval();
+        List<Double> possibleAnswers = new ArrayList<>();
+        for (double i = minValue; i <= maxValue; i += interval) {
+            possibleAnswers.add(i);
         }
-        answerContent.append(CsvConstants.DEFAULT_ARRAY_CLOSE);
-        return answerContent.toString();
+
+        return possibleAnswers.stream()
+                .map(answer -> {
+                    String stringAnswer = String.valueOf(answer);
+                    if (stringAnswer.equals(question.getSuggestedAnswer())) {
+                        stringAnswer += CsvConstants.DEFAULT_SUGGESTED_ANSWER_MARKER;
+                    }
+                    return stringAnswer;
+                })
+                .collect(Collectors.joining(CsvConstants.DEFAULT_POSSIBLE_ANSWERS_DELIMITER));
+    }
+
+    private String getSingleChoiceQuestionCsvAnswers(Question question) {
+        return ((SingleChoiceQuestion) question).getPossibleAnswers().stream()
+                .map(answer -> {
+                    if (Objects.equals(answer, question.getSuggestedAnswer())) {
+                        answer += CsvConstants.DEFAULT_SUGGESTED_ANSWER_MARKER;
+                    }
+                    return answer;
+                })
+                .collect(Collectors.joining(CsvConstants.DEFAULT_POSSIBLE_ANSWERS_DELIMITER));
     }
 
     private String getNumberOfPossibleAnswers(Question question, String type) {
         if (Objects.equals(type, QuestionType.SINGLE_CHOICE.getSymbol())) {
             return String.valueOf(((SingleChoiceQuestion) question).getPossibleAnswers().size());
+        } else if (Objects.equals(type, QuestionType.SCALE.getSymbol())) {
+            return getScaleQuestionPossibleAnswersNumber((ScaleQuestion) question);
         }
-        return DEFAULT_POSSIBLE_ANSWERS_NUMBER;
+        return CsvConstants.DEFAULT_POSSIBLE_ANSWERS_NUMBER;
+    }
+
+    private String getScaleQuestionPossibleAnswersNumber(ScaleQuestion question) {
+        double minValue = question.getMinValue();
+        double maxValue = question.getMaxValue();
+        double interval = question.getInterval();
+        double possibleAnswersNumber = (maxValue - minValue) / interval;
+        return String.valueOf((int) possibleAnswersNumber);
     }
 
 }
